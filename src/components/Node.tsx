8a21001d935d0f36179884f8c402076c2fbdb78d
Rimarok <romaric.ruga@gmail.com>
2021-07-27 23:37:33 +0200

    Node: show 12 DIRECTIONS circle when selected

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,18 +1,49 @@
 import * as React from "react";
+import { DIRECTIONS, DIRECTIONS_MODIFIERS } from "../_data/directions";
+import { BASE_NODE_RADIUS } from "../_data/primordials";
 import INode from "../_interfaces/Node";
 import IOrigin from "../_interfaces/Origin";
 
 function Node({
-  node, origin, selected, select
-}: { node:INode, origin: IOrigin, selected: boolean, select: (selected?: INode) => void }) {
-  const { x: oX, y: oY} = origin
-  const { id, x: nX, y: nY} = node
+  node,
+  origin,
+  selected,
+  select,
+}: {
+  node: INode;
+  origin: IOrigin;
+  selected: boolean;
+  select: (selected?: INode) => void;
+}) {
+  const { x: oX, y: oY } = origin;
+  const { id, x: nX, y: nY } = node;
   const x = oX + nX;
   const y = oY + nY;
-  return <g onClick={() => select(node)}>
-    <text x={x} y={y - 70} textAnchor="middle">{id}</text>
-    <circle cx={x} cy={y} r={50} style={selected ? {stroke: 'red', strokeWidth: 5} : {}}/>
-  </g>
+  return (
+    <g onClick={() => select(node)}>
+      <text x={x} y={y - 70} textAnchor="middle">
+        {id}
+      </text>
+      <circle
+        cx={x}
+        cy={y}
+        r={BASE_NODE_RADIUS * 2}
+        style={selected ? { stroke: "red", strokeWidth: 5 } : {}}
+      />
+      {selected &&
+        Object.values(DIRECTIONS).map((direction) => {
+          const { x: modX, y: modY } = DIRECTIONS_MODIFIERS[direction];
+          return (
+            <circle
+              key={`id-${direction}`}
+              cx={x + modX}
+              cy={y + modY}
+              r={BASE_NODE_RADIUS / 5}
+            ></circle>
+          );
+        })}
+    </g>
+  );
 }
 
-export default Node
+export default Node;
