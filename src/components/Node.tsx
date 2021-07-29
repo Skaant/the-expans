a7e462c5c090a765ae0919765c2933a6bfd0b8be
Rimarok <romaric.ruga@gmail.com>
2021-07-29 07:13:49 +0200

    Node: integrates Building component

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { DIRECTIONS } from "../_data/directions";
-import { BASE_NODE_RADIUS } from "../_data/primordials";
 import NodeModel from "../_models/Node";
 import Coords from "../_models/Coords";
 import NodeSlot from "./NodeSlot";
+import buildings from "../_data/buildings";
+import Building from "./Building";
 
 function Node({
   node,
@@ -17,31 +18,32 @@ function Node({
   select: (selected?: NodeModel) => void;
 }) {
   const { x: originX, y: originY } = origin;
-  const { id, x: nodeX, y: nodeY } = node;
+  const { id, x: nodeX, y: nodeY, buildingId } = node;
   const displayX = originX + nodeX;
   const displayY = originY + nodeY;
+  const building = buildingId && buildings[buildingId];
   return (
     <g onClick={() => select(node)}>
-      <text x={displayX} y={displayY - 70} textAnchor="middle">
-        {id}
-      </text>
-      <circle
-        cx={displayX}
-        cy={displayY}
-        r={BASE_NODE_RADIUS * 2}
-        style={selected ? { stroke: "red", strokeWidth: 5 } : {}}
+      <Building
+        nodeId={id}
+        x={displayX}
+        y={displayY}
+        building={building}
+        selected={selected}
       />
       {selected &&
         Object.values(DIRECTIONS).map((direction) => {
-          return <NodeSlot
-            key={`${id}-${direction}`}
-            source={node}
-            direction={direction}
-            display={{
-              x: displayX,
-              y: displayY
-            }}
-          />
+          return (
+            <NodeSlot
+              key={`${id}-${direction}`}
+              source={node}
+              direction={direction}
+              display={{
+                x: displayX,
+                y: displayY,
+              }}
+            />
+          );
         })}
     </g>
   );
