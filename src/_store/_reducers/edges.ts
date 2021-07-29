b96fe8a7f174b5d412e91180cf6322922f620dc0
Rimarok <romaric.ruga@gmail.com>
2021-07-29 06:09:03 +0200

    Add _reducers/edge

---
+++ src/_store/_reducers/edges.ts
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import EdgeModel from "../../_models/Edge";
+import { addNode } from "./nodes";
+
+export type EdgesState = EdgeModel[];
+
+const initialState: EdgesState = [];
+
+export const edgesSlice = createSlice({
+  name: "edges",
+  initialState,
+  reducers: {},
+  extraReducers: builder =>
+    builder.addCase(addNode, (state, action) => {
+      const {source, node} = action.payload
+      return [
+        ...state,
+        {
+          id: 'azeaze',
+          a: {
+            x: source.x,
+            y: source.y
+          },
+          b: {
+            x: node.x,
+            y: node.y
+          }
+        }
+      ]
+    })
+});
+
+// export const {} = edgesSlice.actions;
+
+export const edgesSelector = (state: RootState) =>
+  state.edges;
+
+export default edgesSlice.reducer;
