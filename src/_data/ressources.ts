90a549bb3b397dd3b119956aea088c991074a1ad
Rimarok <romaric.ruga@gmail.com>
2021-07-29 13:32:54 +0200

    Add _data/ressources (enum RESSOURCES)

---
+++ src/_data/ressources.ts
@@ -0,0 +1,12 @@
+enum RESSOURCES {
+  FAITH = 'faith',
+  ZUMS = 'zums',
+  DATA = 'data',
+  ABS = 'abs',
+  BIO = 'bio',
+  JING = 'jing',
+  SPARK = 'spark',
+  GENO = 'geno'
+}
+
+export default RESSOURCES
