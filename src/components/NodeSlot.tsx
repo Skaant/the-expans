0c8e02696562d1eee4cb7536bdc579b90995c8a1
Rimarok <romaric.ruga@gmail.com>
2021-07-29 07:32:46 +0200

    NodeSlot: randomize addNode building

--- src/components/NodeSlot.tsx
+++ src/components/NodeSlot.tsx
@@ -1,4 +1,6 @@
+import { nanoid } from "@reduxjs/toolkit";
 import * as React from "react";
+import { BUILDINGS } from "../_data/buildings";
 import { DIRECTIONS, DIRECTIONS_MODIFIERS } from "../_data/directions";
 import { BASE_NODE_RADIUS } from "../_data/primordials";
 import Coords from "../_models/Coords";
@@ -9,30 +11,41 @@ import { addNode } from "../_store/_reducers/nodes";
 function NodeSlot({
   source,
   direction,
-  display
-} : {
-  source: NodeModel,
-  direction: DIRECTIONS,
-  display: Coords
+  display,
+}: {
+  source: NodeModel;
+  direction: DIRECTIONS;
+  display: Coords;
 }) {
   const dispatch = useAppDispatch();
   const { x: modX, y: modY } = DIRECTIONS_MODIFIERS[direction];
-  const x = display.x + modX
-  const y = display.y + modY
-  return <circle
-    key={`id-${direction}`}
-    cx={x}
-    cy={y}
-    r={BASE_NODE_RADIUS / 2}
-    onClick={() => dispatch(addNode({
-      source,
-      node: {
-        id: 'SHRINE of Zumbaba',
-        x: source.x + modX,
-        y: source.y + modY
+  const x = display.x + modX;
+  const y = display.y + modY;
+  return (
+    <circle
+      key={`id-${direction}`}
+      cx={x}
+      cy={y}
+      r={BASE_NODE_RADIUS / 2}
+      onClick={() =>
+        dispatch(
+          addNode({
+            source,
+            node: {
+              id: nanoid(),
+              x: source.x + modX,
+              y: source.y + modY,
+              buildingId: [
+                BUILDINGS.SHRINE,
+                BUILDINGS.DOM,
+                BUILDINGS.GRAV_POINT,
+              ][Math.round(Math.random() * 3 - 0.5)],
+            },
+          })
+        )
       }
-    }))}
-  ></circle>
+    ></circle>
+  );
 }
 
-export default NodeSlot
+export default NodeSlot;
