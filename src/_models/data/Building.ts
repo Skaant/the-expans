f2d63caf07c6f18e9aad4b7890c1271328a46775
Rimarok <romaric.ruga@gmail.com>
2021-07-29 13:34:27 +0200

    _models/Building now holds ouputs prop
    Defined private type OutputItem to hold data about buildings' ressources output
    May be generalized for input in the future ?

--- src/_models/data/Building.ts
+++ src/_models/data/Building.ts
@@ -1,10 +1,27 @@
 import { BUILDINGS } from "../../_data/buildings";
+import RESSOURCES from "../../_data/ressources";
+
+type OutputItemType = 'static' | 'relative'
+type OutputItemRelativeConstraints = 'adjacent'
+
+type OutputItem = {
+  ressourceId: RESSOURCES,
+  type: OutputItemType,
+  /** For `type === 'relative'`, `amount` is a coef
+   * for `relativeTo` to be multiplied by. */
+  amount: number,
+  /** Only for `type === 'relative'`. */
+  relativeTo?: RESSOURCES | BUILDINGS,
+  /** Only for `type === 'relative'`. */
+  constraints?: OutputItemRelativeConstraints[]
+}
 
 type BuildingDataModel = {
   id: BUILDINGS;
   name: string;
   radiusModifier?: number;
   strokeColor?: string;
+  outputs?: OutputItem[]
 };
 
 export default BuildingDataModel;
