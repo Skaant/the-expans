838ffa6ac30ee2f3b8d15cdf53a7eca9c3205305
Rimarok <romaric.ruga@gmail.com>
2021-07-29 06:38:13 +0200

    Add components/Edge

--- src/_data/primordials.ts
+++ src/_data/primordials.ts
@@ -1,7 +1,9 @@
 /** In pixel. */
 export const BASE_EDGE_SIZE = 150;
 /** In pixel. */
-export const BASE_NODE_RADIUS = 15;
+export const BASE_NODE_RADIUS = 20;
+/** In pixel. */
+export const BASE_LINE_WIDTH = 10;
 
 export const SIN_30 = 0.5;
 export const COS_30 = 0.8660254;
---
+++ src/components/Edge.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { BASE_LINE_WIDTH } from "../_data/primordials";
+import Coords from "../_models/Coords";
+import EdgeModel from "../_models/Edge";
+
+function Edge({
+  id,
+  a: { x: aX, y: aY },
+  b: { x: bX, y: bY },
+  origin,
+}: EdgeModel & {
+  origin: Coords;
+}) {
+  const displayAX = origin.x + aX;
+  const displayAY = origin.y + aY;
+  const displayBX = origin.x + bX;
+  const displayBY = origin.y + bY;
+  return (
+    <g>
+      <text
+        x={displayAX + (bX - aX) / 2}
+        y={displayAY + (bY - aY) / 2 - 70}
+        textAnchor="middle"
+      >
+        {id}
+      </text>
+      <line
+        x1={displayAX}
+        y1={displayAY}
+        x2={displayBX}
+        y2={displayBY}
+        stroke="grey"
+        strokeWidth={BASE_LINE_WIDTH}
+      />
+    </g>
+  );
+}
+
+export default Edge;
