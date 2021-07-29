f816154555f2fa7d6d241b0ab18ad0c95aaa21fd
Rimarok <romaric.ruga@gmail.com>
2021-07-29 06:06:31 +0200

    Add _models/Edge + _models/Segment
    The first extends the second
    Segment is made of two Coords but could be two nodes in the future

---
+++ src/_models/Edge.ts
@@ -0,0 +1,7 @@
+import Segment from "./Segment";
+
+type EdgeModel = Segment & {
+  id: string;
+}
+
+export default EdgeModel
---
+++ src/_models/Segment.ts
@@ -0,0 +1,9 @@
+import Coords from "./Coords";
+
+/** A path between two points. */
+type Segment = {
+  a: Coords,
+  b: Coords
+}
+
+export default Segment
