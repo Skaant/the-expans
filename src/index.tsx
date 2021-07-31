60bf89b060e179816357c6344d090b12c0a6c9dd
Rimarok <romaric.ruga@gmail.com>
2021-07-31 07:36:51 +0200

    components folder name harmonized to _components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./_components/App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./_store/store";
 import { Provider } from "react-redux";
