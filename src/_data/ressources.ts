474b1942a34907d8aecb799870baed50ae460d9f
Rimarok <romaric.ruga@gmail.com>
2021-07-31 07:19:45 +0200

    ressources: order keys by tier

--- src/_data/ressources.ts
+++ src/_data/ressources.ts
@@ -1,12 +1,31 @@
 enum RESSOURCES {
-  FAITH = 'faith',
-  ZUMS = 'zums',
-  DATA = 'data',
-  ABS = 'abs',
-  BIO = 'bio',
-  JING = 'jing',
-  SPARK = 'spark',
-  GENO = 'geno'
+  // TIER 1
+  /** Water.
+   *
+   * Noted as the base YIN.
+   *
+   * *Used to create buildings
+   *  and to feed ZUMS.* */
+  WA = "wa",
+  /** Material energy.
+   *
+   * Noted as the base YANG.
+   *
+   * *Used to create base buildings
+   *  and to feed ZUMS.*
+   */
+  JING = "jing",
+  /** ZUMS
+   *
+   * Noted as the base BALANCE. */
+  ZUMS = "zums",
+  // TIER 2
+  SPARK = "spark",
+  DATA = "data",
+  FAITH = "faith",
+  // TIER 3
+  ABS = "abs",
+  GENO = "geno",
 }
 
-export default RESSOURCES
+export default RESSOURCES;
