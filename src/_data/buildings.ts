21dffbefa478aac71b7299f2a670492fa0ca91d9
Rimarok <romaric.ruga@gmail.com>
2021-07-31 07:31:19 +0200

    buildings: add WELL and FARM

--- src/_data/buildings.ts
+++ src/_data/buildings.ts
@@ -2,13 +2,119 @@ import BuildingDataModel from "../_models/data/Building";
 import RESSOURCES from "./ressources";
 
 export enum BUILDINGS {
-  HOLY_CITY = "holy-city",
-  SHRINE = "shrine",
+  KOLOS_SEED = "kolos-seed",
+  WELL = "well",
+  FARM = "farm",
   DOM = "dom",
+  SHRINE = "shrine",
   GRAV_POINT = "grav-point",
+  HOLY_CITY = "holy-city",
 }
 
 export default {
+  [BUILDINGS.KOLOS_SEED]: {
+    id: BUILDINGS.KOLOS_SEED,
+    name: "KOLOS-SEED",
+    description:
+      "It felt to the sky, bringing just enough to start a new colony.",
+    outputs: [
+      {
+        ressourceId: RESSOURCES.ZUMS,
+        type: "static",
+        amount: 6,
+      },
+      {
+        ressourceId: RESSOURCES.JING,
+        type: "static",
+        amount: 2,
+      },
+      {
+        ressourceId: RESSOURCES.WA,
+        type: "static",
+        amount: 2,
+      },
+    ],
+  },
+  [BUILDINGS.WELL]: {
+    id: BUILDINGS.WELL,
+    name: "Well",
+    description:
+      "Uses the power of ZUMS and JING to extract WA from the air and ground.",
+    inputs: [
+      {
+        ressourceId: RESSOURCES.ZUMS,
+        amount: 2,
+      },
+      {
+        ressourceId: RESSOURCES.JING,
+        amount: 1,
+      },
+    ],
+    outputs: [
+      {
+        ressourceId: RESSOURCES.WA,
+        type: "static",
+        amount: 4,
+      },
+    ],
+  },
+  [BUILDINGS.FARM]: {
+    id: BUILDINGS.FARM,
+    name: "Farm",
+    description: "ZUMS and WA associate to make the JING grow.",
+    inputs: [
+      {
+        ressourceId: RESSOURCES.ZUMS,
+        amount: 2,
+      },
+      {
+        ressourceId: RESSOURCES.WA,
+        amount: 1,
+      },
+    ],
+    outputs: [
+      {
+        ressourceId: RESSOURCES.JING,
+        type: "static",
+        amount: 4,
+      },
+    ],
+  },
+  [BUILDINGS.DOM]: {
+    id: BUILDINGS.DOM,
+    name: "DOM",
+    description: "WA and JING offer a comfortable living space for ZUMS.",
+    radiusModifier: 1.5,
+    inputs: [
+      {
+        ressourceId: RESSOURCES.WA,
+        amount: 2,
+      },
+      {
+        ressourceId: RESSOURCES.JING,
+        amount: 2,
+      },
+    ],
+    outputs: [
+      {
+        ressourceId: RESSOURCES.ZUMS,
+        type: "static",
+        amount: 6,
+      },
+      {
+        ressourceId: RESSOURCES.JING,
+        type: "static",
+        amount: 3,
+      },
+      {
+        ressourceId: RESSOURCES.SPARK,
+        type: "relative",
+        amount: 1,
+        relativeTo: BUILDINGS.SHRINE,
+        constraints: ["adjacent"],
+      },
+    ],
+  },
   [BUILDINGS.HOLY_CITY]: {
     id: BUILDINGS.HOLY_CITY,
     name: "HOLY CITY of Zumbaba",
@@ -46,37 +152,13 @@ export default {
       },
     ],
   },
-  [BUILDINGS.DOM]: {
-    id: BUILDINGS.DOM,
-    name: "DOM",
-    radiusModifier: 1.5,
-    outputs: [
-      {
-        ressourceId: RESSOURCES.ZUMS,
-        type: "static",
-        amount: 4,
-      },
-      {
-        ressourceId: RESSOURCES.JING,
-        type: "static",
-        amount: 3,
-      },
-      {
-        ressourceId: RESSOURCES.SPARK,
-        type: "relative",
-        amount: 1,
-        relativeTo: BUILDINGS.SHRINE,
-        constraints: ["adjacent"],
-      },
-    ],
-  },
   [BUILDINGS.GRAV_POINT]: {
     id: BUILDINGS.GRAV_POINT,
     name: "GRAV POINT",
     radiusModifier: 2,
     outputs: [
       {
-        ressourceId: RESSOURCES.BIO,
+        ressourceId: RESSOURCES.JING,
         type: "static",
         amount: 5,
       },
