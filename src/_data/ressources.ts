03436a57f4175d4433142b5cdae3cae9fb5a822b
Rimarok <romaric.ruga@gmail.com>
2021-07-31 07:30:20 +0200

    ressource: add RESSOURCES_DATA RessourceDataModel

--- src/_data/ressources.ts
+++ src/_data/ressources.ts
@@ -1,3 +1,5 @@
+import RessourceDataModel from "../_models/data/Ressource";
+
 enum RESSOURCES {
   // TIER 1
   /** Water.
@@ -24,8 +26,43 @@ enum RESSOURCES {
   DATA = "data",
   FAITH = "faith",
   // TIER 3
-  ABS = "abs",
   GENO = "geno",
+  ABS = "abs",
 }
 
 export default RESSOURCES;
+
+export const RESSOURCES_DATA: { [key in RESSOURCES]: RessourceDataModel } = {
+  [RESSOURCES.WA]: {
+    id: RESSOURCES.WA,
+    color: "#34a0a4",
+  },
+  [RESSOURCES.JING]: {
+    id: RESSOURCES.JING,
+    color: "#dc2f02",
+  },
+  [RESSOURCES.ZUMS]: {
+    id: RESSOURCES.ZUMS,
+    color: "#bfd200",
+  },
+  [RESSOURCES.SPARK]: {
+    id: RESSOURCES.SPARK,
+    color: "",
+  },
+  [RESSOURCES.DATA]: {
+    id: RESSOURCES.DATA,
+    color: "",
+  },
+  [RESSOURCES.FAITH]: {
+    id: RESSOURCES.FAITH,
+    color: "",
+  },
+  [RESSOURCES.GENO]: {
+    id: RESSOURCES.GENO,
+    color: "",
+  },
+  [RESSOURCES.ABS]: {
+    id: RESSOURCES.ABS,
+    color: "",
+  },
+};
