a0d03d2e38037a21906928bd13af2312be46a853
Rimarok <romaric.ruga@gmail.com>
2021-08-04 09:45:18 +0200

    _models/NodeSlot: add (direction: DIRECTIONS) prop

--- src/_models/NodeSlot.ts
+++ src/_models/NodeSlot.ts
@@ -1,8 +1,10 @@
+import { DIRECTIONS } from "../_data/directions";
 import Coords from "./Coords";
 import NodeModel from "./Node";
 
 type NodeSlotModel = Coords & {
   id: string;
+  direction: DIRECTIONS;
   /** Node from which slot is emitted. */
   source: NodeModel;
 };
