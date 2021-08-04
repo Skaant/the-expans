8274f7e20ff714310fe09e4f7722c3a853745b1c
Rimarok <romaric.ruga@gmail.com>
2021-08-04 15:06:45 +0200

    _comp/SelectionBuilding: integrate LangDictionnary

--- src/_components/Selection/SelectionBuilding.tsx
+++ src/_components/Selection/SelectionBuilding.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
+import { LANGS } from "../../_data/langs";
 import BuildingDataModel from "../../_models/data/Building";
+import LangDictionnary from "../../_models/LangDictionary";
+import { useAppSelector } from "../../_store/hooks";
+import { langSelector } from "../../_store/_reducers/app";
+
+const langs: LangDictionnary = {
+  building: {
+    [LANGS.FR]: "Bâtiment",
+    [LANGS.EN]: "Building",
+  },
+};
 
 function SelectionBuilding({ building }: { building: BuildingDataModel }) {
+  const lang = useAppSelector(langSelector);
   const [collapse, setCollapse] = useState(true);
   return (
     <div onClick={() => setCollapse(!collapse)}>
       <h2>
-        BUILDING <b>{building.name}</b>
+        {langs["building"][lang]} <b>{building.name}</b>
       </h2>
       <p>{building.description}</p>
       {!collapse && (
