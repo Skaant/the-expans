fe995c5516b59912461e7c11d884e19f5c30a1c5
Rimarok <romaric.ruga@gmail.com>
2021-08-04 18:24:56 +0200

    _models/data/Building: remove relative type
    Every references to "relative" types has been removed from model, because once again WAGNI (now)

--- src/_models/data/Building.ts
+++ src/_models/data/Building.ts
@@ -2,41 +2,14 @@ import { BUILDINGS } from "../../_data/buildings";
 import RESSOURCES from "../../_data/ressources";
 import LangAlt from "../langs/LangAlt";
 
-type ItemRelativeConstraints =
-  /** `adjacent` can be used both with
-   *  `BUILDINGS` and `RESSOURCES`.
-   *
-   * In the case of `RESSOURCES`, it should
-   *  check adjacent nodes' output.
-   *
-   * @note We should see appear `adjacent-n`
-   */
-  | "adjacent"
-  /** can be used both with
-   *  `BUILDINGS` and `RESSOURCES`.
-   *
-   * `network` should check presence of wether
-   *  `BUILDINGS` and `RESSOURCES` presence on
-   *  edge network. */
-  | "network";
-
 type InputItem = {
   ressourceId: RESSOURCES;
   amount: number;
-  constraints?: ItemRelativeConstraints;
 };
-
-type OutputItemType = "static" | "relative";
 
 type OutputItem = {
   ressourceId: RESSOURCES;
-  type: OutputItemType;
-  /** For `type === 'relative'`, `amount` is a coef
-   * for `relativeTo` to be multiplied by. */
   amount: number;
-  /** Only for `type === 'relative'`. */
-  relativeTo?: RESSOURCES | BUILDINGS;
-  constraints?: ItemRelativeConstraints[];
 };
 
 type BuildingDataModel = {
