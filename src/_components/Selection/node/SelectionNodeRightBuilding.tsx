a179017abad3b3f89b9f0fb20b52061e2238985e
Rimarok <romaric.ruga@gmail.com>
2021-08-05 21:26:28 +0200

    _m/SelectionNodeRightBuild: integr ResourcesAmount

--- src/_components/Selection/node/SelectionNodeRightBuilding.tsx
+++ src/_components/Selection/node/SelectionNodeRightBuilding.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useMemo } from "react";
 import { LANGS } from "../../../_data/langs";
 import BuildingDataModel from "../../../_models/data/Building";
 import LangDictionnary from "../../../_models/langs/LangDictionary";
 import { useAppSelector } from "../../../_store/hooks";
 import { langSelector } from "../../../_store/_reducers/app";
+import getTotalSystemsPuts from "../../../_utils/getTotalSystemsPuts";
+import ResourcesAmount from "../../ResourcesAmount";
 
 const langs: LangDictionnary = {
   building: {
@@ -19,25 +22,15 @@ function SelectionNodeRightBuilding({
 }) {
   const lang = useAppSelector(langSelector);
   const [collapse, setCollapse] = useState(true);
+  const total = useMemo(() => getTotalSystemsPuts([building]), [building]);
   return (
     <div onClick={() => setCollapse(!collapse)}>
       <p className="selection-title">{langs["building"][lang]}</p>
       <h2>{building.name[lang]}</h2>
       <p>{building.description[lang]}</p>
-      {!collapse && (
-        <>
-          {building.inputs?.map((input, index) => (
-            <p key={`input-${building.id}-${index}`}>
-              -{input.amount} {input.ressourceId}
-            </p>
-          ))}
-          {building.outputs?.map((output, index) => (
-            <p key={`${building.id}-${index}`}>
-              +{output.amount} {output.ressourceId}
-            </p>
-          ))}
-        </>
-      )}
+      <p>
+        <ResourcesAmount resourcesAmount={total} />
+      </p>
     </div>
   );
 }
