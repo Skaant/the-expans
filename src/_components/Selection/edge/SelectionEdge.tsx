284ad354e0dedc265687419940bcaed9fb21d87d
Rimarok <romaric.ruga@gmail.com>
2021-08-05 12:45:59 +0200

    _comp/SelectionEdge: SelectionEdgeRight integr

--- src/_components/Selection/edge/SelectionEdge.tsx
+++ src/_components/Selection/edge/SelectionEdge.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import SelectionLayout from "../SelectionLayout";
 import { SelectionModel } from "../../../_store/_reducers/selection";
 import SelectionEdgeLeft from "./SelectionEdgeLeft";
+import SelectionEdgeRight from "./SelectionEdgeRight";
+import EdgeModel from "../../../_models/Edge";
 
 function SelectionEdge({ selection }: { selection: SelectionModel }) {
+  const edge = selection.item as EdgeModel;
   return (
     <SelectionLayout
       selection={selection}
       left={<SelectionEdgeLeft selection={selection} />}
+      right={<SelectionEdgeRight edge={edge} />}
     />
   );
 }
