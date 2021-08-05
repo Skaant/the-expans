272eac6c760be8c720acdebc5ca54ed312d7ba28
Rimarok <romaric.ruga@gmail.com>
2021-08-05 22:36:36 +0200

    _actions/nodes: add AddBuildingToNodePayload

--- src/_store/_actions/nodes.ts
+++ src/_store/_actions/nodes.ts
@@ -1,6 +1,12 @@
+import { BUILDINGS } from "../../_data/buildings";
 import NodeModel from "../../_models/Node";
 
 export type AddNodePayload = {
   source: NodeModel;
   node: NodeModel;
-}
+};
+
+export type AddBuildingToNodePayload = {
+  node: NodeModel;
+  buildingId: BUILDINGS;
+};
