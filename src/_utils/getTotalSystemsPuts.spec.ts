756aeb2b18faeb37c2b94e8b1616370c804b719a
Rimarok <romaric.ruga@gmail.com>
2021-08-05 16:01:57 +0200

    _utils/getTotalSystemsPuts.spec: add complete-case

--- src/_utils/getTotalSystemsPuts.spec.ts
+++ src/_utils/getTotalSystemsPuts.spec.ts
@@ -8,4 +8,67 @@ describe("getTotalSystemsPuts", () => {
       Object.values(RESSOURCES).every((ressourceId) => total[ressourceId] === 0)
     ).toEqual(true);
   });
+  it(
+    "complete-case: should return an array with the sum of " +
+      "all inputs and outputs amounts, by ressource id",
+    () => {
+      const total = getTotalSystemsPuts([
+        {
+          inputs: [
+            {
+              ressourceId: RESSOURCES.JING,
+              amount: 2,
+            },
+          ],
+          outputs: [
+            {
+              ressourceId: RESSOURCES.WA,
+              amount: 4,
+            },
+          ],
+        },
+        {
+          inputs: [
+            {
+              ressourceId: RESSOURCES.ZUMS,
+              amount: 3,
+            },
+          ],
+        },
+        {
+          outputs: [
+            {
+              ressourceId: RESSOURCES.JING,
+              amount: 5,
+            },
+          ],
+        },
+        {
+          inputs: [
+            {
+              ressourceId: RESSOURCES.WA,
+              amount: 2,
+            },
+            {
+              ressourceId: RESSOURCES.JING,
+              amount: 1,
+            },
+          ],
+          outputs: [
+            {
+              ressourceId: RESSOURCES.ZUMS,
+              amount: 2,
+            },
+            {
+              ressourceId: RESSOURCES.JING,
+              amount: 3,
+            },
+          ],
+        },
+      ]);
+      expect(total[RESSOURCES.WA]).toEqual(2);
+      expect(total[RESSOURCES.JING]).toEqual(5);
+      expect(total[RESSOURCES.ZUMS]).toEqual(-1);
+    }
+  );
 });
