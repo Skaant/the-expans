305b2c3cf8e27cb6ace31859f47d4a4a6d580f02
Rimarok <romaric.ruga@gmail.com>
2021-08-06 12:34:59 +0200

    _c/Building: display KolosSeedIcon + set building!
    Conditionally displays KOLOS-SEED icon on building.id (generic mechanism has to be determined)

--- src/_components/Building.tsx
+++ src/_components/Building.tsx
@@ -1,24 +1,26 @@
 import * as React from "react";
+import { BUILDINGS } from "../_data/buildings";
 import Coords from "../_models/Coords";
 import BuildingDataModel from "../_models/data/Building";
 import { useAppSelector } from "../_store/hooks";
 import { langSelector } from "../_store/_reducers/app";
+import KolosSeedIcon from "./_icons/KoloSeed.icon";
 
 function Building({
-  nodeId,
   x,
   y,
   building,
-}: { nodeId: string } & Coords & {
-    building?: BuildingDataModel;
-    selected?: boolean;
-  }) {
+}: Coords & {
+  building: BuildingDataModel;
+  selected?: boolean;
+}) {
   const lang = useAppSelector(langSelector);
   return (
     <>
       <text x={x} y={y - 48} textAnchor="middle">
-        {building ? building.name[lang] : ""}
+        {building.name[lang]}
       </text>
+      {building.id === BUILDINGS.KOLOS_SEED && <KolosSeedIcon x={x} y={y} />}
     </>
   );
 }
