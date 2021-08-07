0da1de006069cf3e51d87b6f373b8ca10ec0d731
Rimarok <romaric.ruga@gmail.com>
2021-08-07 12:28:31 +0200

    Add Well.icon

--- src/_components/BuildingIconSwitch.tsx
+++ src/_components/BuildingIconSwitch.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { BUILDINGS } from "../_data/buildings";
 import KolosSeedIcon from "./_icons/KoloSeed.icon";
+import WellIcon from "./_icons/Well.icon";
 import IconPropsModel from "./_icons/_models/IconProps";
 
 function BuildingIconSwitch({
@@ -10,6 +11,8 @@ function BuildingIconSwitch({
   switch (buildingId) {
     case BUILDINGS.KOLOS_SEED:
       return <KolosSeedIcon {...iconProps} />;
+    case BUILDINGS.WELL:
+      return <WellIcon {...iconProps} />;
     default:
       return <></>;
   }
