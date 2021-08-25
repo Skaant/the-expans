151c4b5d511491f2f522e1fd873d5e62674b085e
Rimarok <romaric.ruga@gmail.com>
2021-08-25 17:28:23 +0200

    primordials: add BASE_ICON_SIZE + redu NODE_RADIUS

--- src/_data/primordials.ts
+++ src/_data/primordials.ts
@@ -1,9 +1,11 @@
 /** In pixel. */
 export const BASE_EDGE_SIZE = 150;
 /** In pixel. */
-export const BASE_NODE_RADIUS = 24;
-/** In pixel. */
 export const BASE_LINE_WIDTH = 10;
+/** In pixel. */
+export const BASE_NODE_RADIUS = 15;
+/** In pixel. */
+export const BASE_ICON_SIZE = 48;
 
 export const SIN_30 = 0.5;
 export const COS_30 = 0.8660254;
