ac84ecf1961c622cdbb63171f4254b2d84043b4d
Rimarok <romaric.ruga@gmail.com>
2021-08-25 20:21:37 +0200

    m/Node: add NodeClass to implement NodeModel
    Constructor generates a new nanoid() + a fully initialized edgesRosace (made of EdgeSlotModel)

--- src/_models/Node.ts
+++ src/_models/Node.ts
@@ -1,9 +1,34 @@
+import { nanoid } from "@reduxjs/toolkit";
 import { BUILDINGS } from "../_data/buildings";
+import { DIRECTIONS } from "../_data/directions";
+import getNodeEdgesRosette from "../_utils/getNodeEdgesRosette";
 import Coords from "./Coords";
+import EdgeModel from "./Edge";
+import EdgeSlotModel from "./EdgeSlot";
+
+export type NodeEdgesRosette = {
+  [key in DIRECTIONS]: EdgeModel | EdgeSlotModel;
+};
 
 type NodeModel = Coords & {
   id: string;
   buildingId?: BUILDINGS;
+  edgesRosette?: NodeEdgesRosette;
 };
+
+export class NodeClass implements NodeModel {
+  x: number;
+  y: number;
+  id: string;
+  buildingId?: BUILDINGS;
+  edgesRosette: NodeEdgesRosette;
+  constructor({ x, y, buildingId }: Omit<NodeModel, "id">) {
+    this.x = x;
+    this.y = y;
+    this.id = nanoid();
+    this.buildingId = buildingId;
+    this.edgesRosette = getNodeEdgesRosette(this);
+  }
+}
 
 export default NodeModel;
