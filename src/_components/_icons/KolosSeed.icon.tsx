81d822ed7d5f559e0bba97210a1f4a5f813652a1
Rimarok <romaric.ruga@gmail.com>
2021-08-25 17:27:49 +0200

    KolosSeed.icon: fix name + update content

---
+++ src/_components/_icons/KolosSeed.icon.tsx
@@ -0,0 +1,184 @@
+import * as React from "react";
+import Icon from "./Icon";
+import IconPropsModel from "./_models/IconProps";
+
+/**
+ * @copyright https://game-icons.net/1x1/lorc/acorn.html
+ */
+function KolosSeedIcon(props: IconPropsModel) {
+  return (
+    <Icon {...props}>
+      <>
+        <defs>
+          <path
+            d="M207.86 474.51C224.26 476.88 238.7 476.88 251.2 474.51C237.01 406.37 251.36 371.12 294.25 368.75C337.14 366.39 353.74 398.4 344.03 464.77C359.07 464.77 373.19 463.29 386.38 460.33C399.57 457.36 411.83 452.92 423.18 446.99C441.11 409.2 442.59 350.51 427.62 270.92C405.17 151.54 282.68 133.09 260.45 108.19C238.22 83.29 229.55 47.5 222.21 48.61C214.87 49.72 222.21 83.29 209.76 119.75C197.31 156.21 135.06 161.54 113.72 256.69C99.5 320.12 112.54 388 152.85 460.33C173.13 467.42 191.47 472.15 207.86 474.51Z"
+            id="a2xTJWEaKd"
+          ></path>
+          <path
+            d="M251.55 296.71C228.43 287.82 199.68 274.18 165.3 255.8C130.91 237.43 87.05 195.34 33.69 129.53"
+            id="itbBLQqLu"
+          ></path>
+          <path
+            d="M230.21 324.27C195.24 319.53 161.15 310.34 127.95 296.71C94.75 283.07 60.96 253.14 26.58 206.9"
+            id="alAi4tJUy"
+          ></path>
+          <path
+            d="M453.41 134.87C436.81 175.77 418.43 209.86 398.28 237.13C378.12 264.4 347.29 283.07 305.8 293.15"
+            id="cwLIWq48i"
+          ></path>
+          <path
+            d="M485.42 224.68C472.97 248.99 457.11 270.92 437.84 290.48C418.56 310.05 379.59 322.79 320.91 328.72"
+            id="i247FS6Gw1"
+          ></path>
+          <path
+            d="M249.78 188.22L203.54 173.1L203.54 215.79L246.22 232.68L249.78 188.22Z"
+            id="bamTbR8BP"
+          ></path>
+          <path
+            d="M352.04 220.23L345.81 176.66L303.13 190L303.13 232.68L352.04 220.23Z"
+            id="a1ycmhgywV"
+          ></path>
+          <path
+            d="M299.57 133.98L284.56 89.52L249.78 125.09L270.23 156.21L299.57 133.98Z"
+            id="d61Hf0VQZK"
+          ></path>
+        </defs>
+        <g>
+          <g>
+            <g>
+              <g>
+                <filter
+                  id="shadow5198575"
+                  x="99.16"
+                  y="39.59"
+                  width="348.68"
+                  height="446.7"
+                  filterUnits="userSpaceOnUse"
+                  primitiveUnits="userSpaceOnUse"
+                >
+                  <feFlood></feFlood>
+                  <feComposite in2="SourceAlpha" operator="in"></feComposite>
+                  <feGaussianBlur stdDeviation="1"></feGaussianBlur>
+                  <feOffset dx="1" dy="1" result="afterOffset"></feOffset>
+                  <feFlood flood-color="#000000" flood-opacity="0.5"></feFlood>
+                  <feComposite in2="afterOffset" operator="in"></feComposite>
+                  <feMorphology operator="dilate" radius="1"></feMorphology>
+                  <feComposite in2="SourceAlpha" operator="out"></feComposite>
+                </filter>
+                <path
+                  d="M207.86 474.51C224.26 476.88 238.7 476.88 251.2 474.51C237.01 406.37 251.36 371.12 294.25 368.75C337.14 366.39 353.74 398.4 344.03 464.77C359.07 464.77 373.19 463.29 386.38 460.33C399.57 457.36 411.83 452.92 423.18 446.99C441.11 409.2 442.59 350.51 427.62 270.92C405.17 151.54 282.68 133.09 260.45 108.19C238.22 83.29 229.55 47.5 222.21 48.61C214.87 49.72 222.21 83.29 209.76 119.75C197.31 156.21 135.06 161.54 113.72 256.69C99.5 320.12 112.54 388 152.85 460.33C173.13 467.42 191.47 472.15 207.86 474.51Z"
+                  id="a1AMe7svMH"
+                  fill="white"
+                  fill-opacity="1"
+                  filter="url(#shadow5198575)"
+                ></path>
+              </g>
+              <use
+                xlinkHref="#a2xTJWEaKd"
+                opacity="1"
+                fill="#ffffff"
+                fill-opacity="1"
+              ></use>
+              <g>
+                <use
+                  xlinkHref="#a2xTJWEaKd"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#444444"
+                  stroke-width="13.33"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#itbBLQqLu"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#alAi4tJUy"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#cwLIWq48i"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#i247FS6Gw1"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#bamTbR8BP"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#a1ycmhgywV"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+            <g>
+              <g>
+                <use
+                  xlinkHref="#d61Hf0VQZK"
+                  opacity="1"
+                  fill-opacity="0"
+                  stroke="#92fd7a"
+                  stroke-width="20"
+                  stroke-opacity="1"
+                ></use>
+              </g>
+            </g>
+          </g>
+        </g>
+      </>
+    </Icon>
+  );
+}
+
+export default KolosSeedIcon;
