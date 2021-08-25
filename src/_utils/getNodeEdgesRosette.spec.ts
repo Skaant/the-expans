bcc32ed0a20e4cf862a9fec87bb419b40753b6d3
Rimarok <romaric.ruga@gmail.com>
2021-08-25 22:35:19 +0200

    getNodeEdgesRosette: adapt to new rosette model
    First use of type prop check

--- src/_utils/getNodeEdgesRosette.spec.ts
+++ src/_utils/getNodeEdgesRosette.spec.ts
@@ -1,16 +1,19 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { DIRECTIONS } from "../_data/directions";
 import { EDGE_TYPES } from "../_data/edges";
+import EdgeSlotModel from "../_models/EdgeSlot";
 import getNodeEdgesRosette from "./getNodeEdgesRosette";
 
 describe("getNodeEdgesRosette", () => {
   test("Return an object made of EdgeSlotModel & keyed for each DIRECTIONS", () => {
     const edgesRosette = getNodeEdgesRosette({ x: 0, y: 0, id: nanoid() });
     expect(
-      Object.values(DIRECTIONS).every((direction) =>
-        [EDGE_TYPES.GROUND, EDGE_TYPES.GROUND_IMPRACTICABLE].includes(
-          edgesRosette[direction].type
-        )
+      Object.values(DIRECTIONS).every(
+        (direction) =>
+          edgesRosette[direction].type === "edge-slot" &&
+          [EDGE_TYPES.GROUND, EDGE_TYPES.GROUND_IMPRACTICABLE].includes(
+            (edgesRosette[direction].value as EdgeSlotModel).type
+          )
       )
     ).toEqual(true);
   });
--- src/_utils/getNodeEdgesRosette.ts
+++ src/_utils/getNodeEdgesRosette.ts
@@ -1,18 +1,21 @@
 import { DIRECTIONS } from "../_data/directions";
 import { EDGE_TYPES } from "../_data/edges";
 import EdgeSlotModel from "../_models/EdgeSlot";
-import NodeModel from "../_models/Node";
+import NodeModel, { NodeEdgesRosetteModel } from "../_models/Node";
 
-function getNodeEdgesRosette(source: NodeModel) {
+function getNodeEdgesRosette(source: Omit<NodeModel, "edgesRosette">) {
   return Object.values(DIRECTIONS).reduce((rosette, direction) => {
     const rand = Math.round(Math.random() * 10);
     rosette[direction] = {
-      source,
-      direction,
-      type: rand > 7 ? EDGE_TYPES.GROUND : EDGE_TYPES.GROUND_IMPRACTICABLE,
-    } as EdgeSlotModel;
+      type: "edge-slot",
+      value: {
+        sourceId: source.id,
+        direction,
+        type: rand < 7 ? EDGE_TYPES.GROUND : EDGE_TYPES.GROUND_IMPRACTICABLE,
+      } as EdgeSlotModel,
+    };
     return rosette;
-  }, {} as { [key in DIRECTIONS]: EdgeSlotModel });
+  }, {} as NodeEdgesRosetteModel);
 }
 
 export default getNodeEdgesRosette;
