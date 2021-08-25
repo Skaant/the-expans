999a552a290addb407454d8e1f0b577d4989041e
Rimarok <romaric.ruga@gmail.com>
2021-08-25 17:30:15 +0200

    IconProps: divide ICON_SIZE_MOD. by two
    Adapt to BASE_ICON_SIZE

--- src/_components/_icons/_models/IconProps.ts
+++ src/_components/_icons/_models/IconProps.ts
@@ -5,9 +5,9 @@ export enum ICON_SIZES {
 }
 
 export const ICON_SIZES_MODIFIER: { [key in ICON_SIZES]: number } = {
-  [ICON_SIZES.SM]: 1,
-  [ICON_SIZES.MD]: 2,
-  [ICON_SIZES.LG]: 4,
+  [ICON_SIZES.SM]: 0.5,
+  [ICON_SIZES.MD]: 1,
+  [ICON_SIZES.LG]: 2,
 };
 
 type IconPropsModel = {
