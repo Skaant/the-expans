b4e0cbcdb77efb3f1f7fe0820796fe96a8e715d5
Rimarok <romaric.ruga@gmail.com>
2021-08-25 20:22:08 +0200

    r/nodes: use NodeClass constr for initialState

--- src/_store/_reducers/nodes.ts
+++ src/_store/_reducers/nodes.ts
@@ -1,18 +1,13 @@
 import { AddBuildingToNodePayload, AddNodePayload } from "../_actions/nodes";
-import NodeModel from "../../_models/Node";
-import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
+import NodeModel, { NodeClass } from "../../_models/Node";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 import { BUILDINGS } from "../../_data/buildings";
 
 export type NodesState = NodeModel[];
 
 const initialState: NodesState = [
-  {
-    id: nanoid(),
-    x: 0,
-    y: 0,
-    buildingId: BUILDINGS.KOLOS_SEED,
-  },
+  new NodeClass({ x: 0, y: 0, buildingId: BUILDINGS.KOLOS_SEED }),
 ];
 
 export const nodesSlice = createSlice({
