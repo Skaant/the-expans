94ee05f17b9db173afb776559d9d4ada31ee2cb2
Rimarok <romaric.ruga@gmail.com>
2021-08-26 22:46:55 +0200

    primordials: updated edge size & line width

--- src/_data/primordials.ts
+++ src/_data/primordials.ts
@@ -1,7 +1,7 @@
 /** In pixel. */
-export const BASE_EDGE_SIZE = 150;
+export const BASE_EDGE_SIZE = 240;
 /** In pixel. */
-export const BASE_LINE_WIDTH = 10;
+export const BASE_LINE_WIDTH = 6;
 /** In pixel. */
 export const BASE_NODE_RADIUS = 15;
 /** In pixel. */
