5a73550a6347c47f3ab7df963ac061606a0ad4bf
Rimarok <romaric.ruga@gmail.com>
2021-08-27 09:43:41 +0200

    _c/KoloSeed.icon.tsx: update model

--- src/_components/_icons/KolosSeed.icon.tsx
+++ src/_components/_icons/KolosSeed.icon.tsx
@@ -2,90 +2,60 @@ import * as React from "react";
 import Icon from "./Icon";
 import IconPropsModel from "./_models/IconProps";
 
-/**
- * @copyright https://game-icons.net/1x1/lorc/acorn.html
- */
 function KolosSeedIcon(props: IconPropsModel) {
   return (
-    <Icon {...props}>
+    <Icon {...props} viewBox="0 0 128 128">
       <>
         <defs>
           <path
-            d="M207.86 474.51C224.26 476.88 238.7 476.88 251.2 474.51C237.01 406.37 251.36 371.12 294.25 368.75C337.14 366.39 353.74 398.4 344.03 464.77C359.07 464.77 373.19 463.29 386.38 460.33C399.57 457.36 411.83 452.92 423.18 446.99C441.11 409.2 442.59 350.51 427.62 270.92C405.17 151.54 282.68 133.09 260.45 108.19C238.22 83.29 229.55 47.5 222.21 48.61C214.87 49.72 222.21 83.29 209.76 119.75C197.31 156.21 135.06 161.54 113.72 256.69C99.5 320.12 112.54 388 152.85 460.33C173.13 467.42 191.47 472.15 207.86 474.51Z"
-            id="a2xTJWEaKd"
+            d="M39.34 114.85C43.25 115.74 50.36 116.44 60.69 116.94C53.54 95.69 57.32 85 72.05 84.86C86.78 84.71 91.11 95.41 85.06 116.94C88.07 116.62 91.19 116.24 94.43 115.78C97.67 115.33 100.84 113.93 103.95 111.59C111.55 95.56 110.63 77.7 101.21 58.01C87.07 28.48 65.55 34.36 60.69 27.92C55.82 21.49 53.63 11.61 51.84 11.74C50.05 11.86 51.16 21.98 47.44 30.52C43.73 39.06 35.09 36.16 28.08 58.66C23.41 73.66 23.41 91.1 28.08 110.97C31.69 112.67 35.44 113.96 39.34 114.85Z"
+            id="a6JLOZ69X1"
           ></path>
           <path
-            d="M251.55 296.71C228.43 287.82 199.68 274.18 165.3 255.8C130.91 237.43 87.05 195.34 33.69 129.53"
-            id="itbBLQqLu"
+            d="M64.33 67.18C63.51 66.36 60.61 64.29 55.65 60.98C48.2 56 38.86 55.18 27.84 45.12C20.49 38.41 15.33 31.81 12.35 25.31"
+            id="a41uNKBcw6"
           ></path>
           <path
-            d="M230.21 324.27C195.24 319.53 161.15 310.34 127.95 296.71C94.75 283.07 60.96 253.14 26.58 206.9"
-            id="alAi4tJUy"
+            d="M60.83 74.24C59.15 73.84 56.08 72.94 51.6 71.52C44.87 69.39 40.11 70.21 28.82 65.99C21.29 63.17 14.45 59.38 8.31 54.63"
+            id="ctT1TFA10"
           ></path>
           <path
-            d="M453.41 134.87C436.81 175.77 418.43 209.86 398.28 237.13C378.12 264.4 347.29 283.07 305.8 293.15"
-            id="cwLIWq48i"
+            d="M113.59 28.17C111 36.69 107.16 43.76 102.07 49.39C94.43 57.83 90.5 55.6 83.84 59.78C79.41 62.56 76.07 65.03 73.84 67.18"
+            id="b1EzcjKmib"
           ></path>
           <path
-            d="M485.42 224.68C472.97 248.99 457.11 270.92 437.84 290.48C418.56 310.05 379.59 322.79 320.91 328.72"
-            id="i247FS6Gw1"
+            d="M122.57 49.28C119.1 54.89 114.85 59.86 109.82 64.2C105.57 67.86 93.95 68.74 87.19 70.84C82.69 72.24 79.64 73.94 78.04 75.96"
+            id="a3VWySpcd"
           ></path>
           <path
-            d="M249.78 188.22L203.54 173.1L203.54 215.79L246.22 232.68L249.78 188.22Z"
-            id="bamTbR8BP"
+            d="M72.22 39.47L58.98 28.17L53.16 42.29L64.98 53.7L72.22 39.47Z"
+            id="b2oR7zSUtU"
           ></path>
           <path
-            d="M352.04 220.23L345.81 176.66L303.13 190L303.13 232.68L352.04 220.23Z"
-            id="a1ycmhgywV"
+            d="M95.24 42.37L86.85 30.06L75.88 40.48L82.24 53.44L95.24 42.37Z"
+            id="a26daEMNll"
           ></path>
           <path
-            d="M299.57 133.98L284.56 89.52L249.78 125.09L270.23 156.21L299.57 133.98Z"
-            id="d61Hf0VQZK"
+            d="M84.19 20.58L79.84 3.12L65.43 15.63L72.28 28.17L84.19 20.58Z"
+            id="cS7era71i"
           ></path>
         </defs>
         <g>
           <g>
             <g>
-              <g>
-                <filter
-                  id="shadow5198575"
-                  x="99.16"
-                  y="39.59"
-                  width="348.68"
-                  height="446.7"
-                  filterUnits="userSpaceOnUse"
-                  primitiveUnits="userSpaceOnUse"
-                >
-                  <feFlood></feFlood>
-                  <feComposite in2="SourceAlpha" operator="in"></feComposite>
-                  <feGaussianBlur stdDeviation="1"></feGaussianBlur>
-                  <feOffset dx="1" dy="1" result="afterOffset"></feOffset>
-                  <feFlood flood-color="#000000" flood-opacity="0.5"></feFlood>
-                  <feComposite in2="afterOffset" operator="in"></feComposite>
-                  <feMorphology operator="dilate" radius="1"></feMorphology>
-                  <feComposite in2="SourceAlpha" operator="out"></feComposite>
-                </filter>
-                <path
-                  d="M207.86 474.51C224.26 476.88 238.7 476.88 251.2 474.51C237.01 406.37 251.36 371.12 294.25 368.75C337.14 366.39 353.74 398.4 344.03 464.77C359.07 464.77 373.19 463.29 386.38 460.33C399.57 457.36 411.83 452.92 423.18 446.99C441.11 409.2 442.59 350.51 427.62 270.92C405.17 151.54 282.68 133.09 260.45 108.19C238.22 83.29 229.55 47.5 222.21 48.61C214.87 49.72 222.21 83.29 209.76 119.75C197.31 156.21 135.06 161.54 113.72 256.69C99.5 320.12 112.54 388 152.85 460.33C173.13 467.42 191.47 472.15 207.86 474.51Z"
-                  id="a1AMe7svMH"
-                  fill="white"
-                  fill-opacity="1"
-                  filter="url(#shadow5198575)"
-                ></path>
-              </g>
               <use
-                xlinkHref="#a2xTJWEaKd"
+                xlinkHref="#a6JLOZ69X1"
                 opacity="1"
                 fill="#ffffff"
                 fill-opacity="1"
               ></use>
               <g>
                 <use
-                  xlinkHref="#a2xTJWEaKd"
+                  xlinkHref="#a6JLOZ69X1"
                   opacity="1"
                   fill-opacity="0"
                   stroke="#444444"
-                  stroke-width="13.33"
+                  stroke-width="5"
                   stroke-opacity="1"
                 ></use>
               </g>
@@ -93,11 +63,11 @@ function KolosSeedIcon(props: IconPropsModel) {
             <g>
               <g>
                 <use
-                  xlinkHref="#itbBLQqLu"
+                  xlinkHref="#a41uNKBcw6"
                   opacity="1"
                   fill-opacity="0"
                   stroke="#92fd7a"
-                  stroke-width="20"
+                  stroke-width="5"
                   stroke-opacity="1"
                 ></use>
               </g>
@@ -105,11 +75,11 @@ function KolosSeedIcon(props: IconPropsModel) {
             <g>
               <g>
                 <use
-                  xlinkHref="#alAi4tJUy"
+                  xlinkHref="#ctT1TFA10"
                   opacity="1"
                   fill-opacity="0"
                   stroke="#92fd7a"
-                  stroke-width="20"
+                  stroke-width="5"
                   stroke-opacity="1"
                 ></use>
               </g>
@@ -117,11 +87,11 @@ function KolosSeedIcon(props: IconPropsModel) {
             <g>
               <g>
                 <use
-                  xlinkHref="#cwLIWq48i"
+                  xlinkHref="#b1EzcjKmib"
                   opacity="1"
                   fill-opacity="0"
                   stroke="#92fd7a"
-                  stroke-width="20"
+                  stroke-width="5"
                   stroke-opacity="1"
                 ></use>
               </g>
@@ -129,50 +99,38 @@ function KolosSeedIcon(props: IconPropsModel) {
             <g>
               <g>
                 <use
-                  xlinkHref="#i247FS6Gw1"
+                  xlinkHref="#a3VWySpcd"
                   opacity="1"
                   fill-opacity="0"
                   stroke="#92fd7a"
-                  stroke-width="20"
+                  stroke-width="5"
                   stroke-opacity="1"
                 ></use>
               </g>
             </g>
             <g>
-              <g>
-                <use
-                  xlinkHref="#bamTbR8BP"
-                  opacity="1"
-                  fill-opacity="0"
-                  stroke="#92fd7a"
-                  stroke-width="20"
-                  stroke-opacity="1"
-                ></use>
-              </g>
+              <use
+                xlinkHref="#b2oR7zSUtU"
+                opacity="1"
+                fill="#92fd7a"
+                fill-opacity="1"
+              ></use>
             </g>
             <g>
-              <g>
-                <use
-                  xlinkHref="#a1ycmhgywV"
-                  opacity="1"
-                  fill-opacity="0"
-                  stroke="#92fd7a"
-                  stroke-width="20"
-                  stroke-opacity="1"
-                ></use>
-              </g>
+              <use
+                xlinkHref="#a26daEMNll"
+                opacity="1"
+                fill="#92fd7a"
+                fill-opacity="1"
+              ></use>
             </g>
             <g>
-              <g>
-                <use
-                  xlinkHref="#d61Hf0VQZK"
-                  opacity="1"
-                  fill-opacity="0"
-                  stroke="#92fd7a"
-                  stroke-width="20"
-                  stroke-opacity="1"
-                ></use>
-              </g>
+              <use
+                xlinkHref="#cS7era71i"
+                opacity="1"
+                fill="#92fd7a"
+                fill-opacity="1"
+              ></use>
             </g>
           </g>
         </g>
