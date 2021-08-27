e72b54e051b59e98869c62e6de6ab7459576efe9
Rimarok <romaric.ruga@gmail.com>
2021-08-27 18:56:56 +0200

    getNodeEdgesRosette: generate w/ new EDGE_TYPES

--- src/_utils/getNodeEdgesRosette.ts
+++ src/_utils/getNodeEdgesRosette.ts
@@ -5,13 +5,20 @@ import NodeModel, { NodeEdgesRosetteModel } from "../_models/Node";
 
 function getNodeEdgesRosette(source: Omit<NodeModel, "edgesRosette">) {
   return Object.values(DIRECTIONS).reduce((rosette, direction) => {
-    const rand = Math.round(Math.random() * 10);
+    const rand = Math.round(Math.random() * 100);
     rosette[direction] = {
       type: "edge-slot",
       value: {
         sourceId: source.id,
         direction,
-        type: rand < 7 ? EDGE_TYPES.GROUND : EDGE_TYPES.GROUND_IMPRACTICABLE,
+        type:
+          rand < 70
+            ? rand < 50
+              ? EDGE_TYPES.PLAIN
+              : EDGE_TYPES.MOUNTAIN
+            : rand < 90
+            ? EDGE_TYPES.WATER
+            : EDGE_TYPES.SKIGH,
       } as EdgeSlotModel,
     };
     return rosette;
