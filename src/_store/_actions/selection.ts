f4cc8413a2fe83ecd7edff3d5fd4ef9a999b90c4
Rimarok <romaric.ruga@gmail.com>
2021-08-27 10:22:11 +0200

    _actions: add source? to SelectPayload

--- src/_store/_actions/selection.ts
+++ src/_store/_actions/selection.ts
@@ -1,4 +1,7 @@
+import NodeModel from "../../_models/Node";
 import { SelectionModel } from "../_reducers/selection";
 export interface SelectPayload {
   next?: SelectionModel;
+  /** Back to node on edge-slot (re-de)selection. */
+  source?: NodeModel;
 }
